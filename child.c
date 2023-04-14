94fddd98c84be6a9889f93e468c8410f18a25606
Leo Sohler <lsohler@c1r3s7.42lausanne.ch>
2023-04-14 18:09:37 +0200

    child

--- child.c
+++ child.c
@@ -3,65 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   child.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsohler@student.42.fr <lsohler>            +#+  +:+       +#+        */
+/*   By: lsohler <lsohler@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 12:59:21 by lsohler@stu       #+#    #+#             */
-/*   Updated: 2023/03/24 15:35:15 by lsohler@stu      ###   ########.fr       */
+/*   Updated: 2023/04/14 15:54:56 by lsohler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	pipex_child(px_list *child)
+void	my_dup2(px_list *child)
 {
-	pid_t	pid;
-	
-	if (child->child_n == 1)
-	{
-		dup2
-	}
-	else if (child->child_n == -1)
-
-	else
+		dup2();
+		dup2();
+		close();
 }
 
-	while (*cmd != NULL) 
+void	pipex_child(px_list *child, f_list *file, char **envp)
+{
+	child->pid = fork();
+	if (child->pid == -1)
+		error_msg("fork error\n");
+	if (child->child_n == 1)
 	{
-		if ((pid = fork()) == -1) 
-		{
-			perror("fork");
-			exit(1);
-			}
-		else if (pid == 0)
-		{
-		// if there is next
-		if (*(cmd + 1) != NULL)
-		{
-			if (dup2(fd[j + 1], 1) < 0)
-			{
-				perror("dup2");
-				exit(EXIT_FAILURE);
-			}
-		}
-		// if there is previous
-		if (j != 0)
-		{
-			if (dup2(fd[j - 2], 0) < 0)
-			{
-				perror("dup2");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-      for (i = 0; i < 2 * cmd_len; i++) {
-        close(fd[i]);
-      }
-
-      if (execvp((*cmd)[0], *cmd) < 0) {
-        perror((*cmd)[0]);
-        exit(EXIT_FAILURE);
-      }
-    } else if (pid < 0) {
-      perror("error");
-      exit(EXIT_FAILURE);
-    }
+		dup2(file->infile, 0);
+		dup2(child->fd[0], 1);
+	}
+	else if (child->child_n == -1)
+	{
+		dup2(child->prev->fd[1], 0);
+		dup2(file->outfile, 1);
+	}
+	else
+	{
+		dup2(child->prev->fd[1], 0);
+		dup2(child->fd[0], 1);
+	}
+	close(child->fd[0]);
+	execve(child->path, child->cmd, envp);
+}
